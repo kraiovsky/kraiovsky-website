2dd2deb4fa29f5339b3e885971c445f83152fba8
Roman Krayovskyy <rkrayovskyy@gmail.com>
2020-05-03 11:18:48 +0200

    Minor refactor

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,10 @@
 import { Provider } from 'react-redux'
 import PropTypes from 'prop-types'
-import Wrapper from '../components/Wrapper'
 import { config as faConfig } from '@fortawesome/fontawesome-svg-core'
 import '@fortawesome/fontawesome-svg-core/styles.css'
 import '../styles/style.scss'
 import store from '../store'
+import Wrapper from '../components/Wrapper'
 
 faConfig.autoAddCss = false
 
